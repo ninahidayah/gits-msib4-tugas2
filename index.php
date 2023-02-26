599eba31e75430d7111d4cfef39c62318aead830
ninahidayah <ninaahidayahh@gmail.com>
2023-02-26 20:31:57 +0700

    second commit

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 </head>
 <body>
     <nav>
-        <h5 class="logo">Portofolio</h5>
+        <h5 class="logo">Portofolio Nina</h5>
         <ul>
            <li><a href="#home">Home</a></li>
            <li><a href="#about">About</a></li>
