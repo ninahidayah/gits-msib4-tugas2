c354a98af578a90f83144546da3c6caa31c58320
ninahidayah <ninaahidayahh@gmail.com>
2023-02-26 20:12:53 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portofolio</title>
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="aset/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+</head>
+<body>
+    <nav>
+        <h5 class="logo">Portofolio</h5>
+        <ul>
+           <li><a href="#home">Home</a></li>
+           <li><a href="#about">About</a></li>
+           <li><a href="#skill">Skill</a></li>
+           <li><a href="#contact">Contact</a></li>
+        </ul>
+        <a href="#"></a>
+    </nav>
+    <div class="home" id="home"> 
+        <div class="center">
+            <img src="foto.png" alt="">
+        </div>
+        <div class="center">
+            <h1>WELCOME</h1>
+            <h4>Nina Hidayah's Profil Website</h4>
+            <h4> Universitas PGRI Ronggolawe Tuban</h4>
+            <a href="#about">More About Me</a>
+        </div>
+    </div>
+    <section class="about" id="about">
+        <div class="aboutme">
+            <img src="foto.png" alt="">
+            <div class="abouttext">
+                <h2>About Me</h2>
+            <h5>
+                    <p>I am an Informatics Engineering student
+                    at PGRI Ronggolawe University Tuban. 
+                    Interested in web app development. 
+                    currently following an experience to add 
+                    to my skills on an Independent Study program at PT gits Indonesia.</p>
+                    <a href="#contact"class="button">Contact Me</a>
+            </h5>
+
+        </div>
+    </section>
+
+    <section id="skill" class="skill">
+        <div class="container">
+            <div class="row">
+                <h1 class="text-center">Skills</h1>
+            </div>
+            <!-- untuk content ke tengah -->
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="skill-item">
+                        <h5><i class="bi bi-book-half"></i>  Language :</h5>
+                        <div class="ps-4">
+                            <p><i class="bi bi-flag"></i> Indonesian</p>
+                            <div class="progress mb-2" style="height: 19px;">
+                            <div class="progress-bar" role="progressbar" style="width: 100%; background-color: blue" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <p><i class="bi bi-flag"></i> English</p>
+                            <div class="progress mb-2" style="height: 19px;">
+                            <div class="progress-bar" role="progressbar" style="width: 60%; background-color: blue" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                    <div class="skill-item">
+                        <h5><i class="bi bi-book-half"></i>  Tech Stack :</h5>
+                        <div class="ps-4">
+                            <p><i class="bi bi-filetype-html"></i> HTML</p>
+                            <div class="progress mb-2" style="height: 19px;">
+                            <div class="progress-bar" role="progressbar" style="width: 80%; background-color: blue" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <p> <i class="bi bi-filetype-css"></i> CSS</p>
+                            <div class="progress mb-2" style="height: 19px;">
+                            <div class="progress-bar" role="progressbar" style="width: 80%; background-color: blue" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <p> <i class="bi bi-filetype-js"></i> Javascript</p>
+                            <div class="progress mb-2" style="height: 19px;">
+                            <div class="progress-bar" role="progressbar" style="width: 53%; background-color: blue" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <p> <i class="bi bi-filetype-php"></i> PHP</p>
+                            <div class="progress mb-2" style="height: 19px;">
+                            <div class="progress-bar" role="progressbar" style="width: 50%; background-color: blue" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="contact" class="contact">
+        <div class="container">
+        <div class="row">
+                <h1 class="text-center mb-5">Contact</h1>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="card text-center" style="width: 18rem;">
+                        <div class="card-body">
+                            <i class="bi bi-telephone-inbound-fill" style="font-size:50px; color: blue"></i>
+                            <h6 class="card-subtitle mb-2 text-muted">Phone</h6>
+                            <p class="card-text"><a href="https://wa.me/085859850875">(+62)85859850875</a></p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                        <div class="card text-center" style="width: 18rem;">
+                        <div class="card-body">
+                            <i class="bi bi-envelope" style="font-size:50px; color: blue"></i>
+                            <h6 class="card-subtitle mb-2 text-muted">Gmail</h6>
+                            <p class="card-text">
+                            <a href="mailto:ninaahidayahh@gmail.com">ninaahidayahh@gmail.com</a></p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card text-center" style="width: 18rem;">
+                        <div class="card-body">
+                            <i class="bi bi-instagram" style="font-size:50px; color: blue"></i>
+                            <h6 class="card-subtitle mb-2 text-muted">Instagram</h6>
+                            <p class="card-text"><a href="https://www.instagram.com/ninahidayaah">ninahidayaah</a></p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card text-center" style="width: 18rem;">
+                        <div class="card-body">
+                            <i class="bi bi-twitter" style="font-size:50px; color: blue"></i>
+                            <h6 class="card-subtitle mb-2 text-muted">twitter</h6>
+                            <p class="card-text"><a href="twitter.com/ninahidayahh">ninaahidayahh</a></p>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </section>
+</body>
+</html>
